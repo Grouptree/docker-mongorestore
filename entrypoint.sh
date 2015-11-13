6d1cf36ecbbe0fc16fc7d909ba1d456536f02bfd
Potiguar Faga <potz@potz.me>
2015-11-13 14:14:18 +0000

    Allow extra command-line arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! "$1" ] || [ ! "$2"  ]; then
-  echo "Usage: docker run grouptree/mongorestore <host> <database>."
+  echo "Usage: docker run grouptree/mongorestore <host> <database> <args>"
   exit 1
 fi
 
@@ -13,6 +13,6 @@ fi
 
 cd /tmp
 tar zxf /backups/$2/latest.tar.gz .
-mongorestore --batchSize=10 --drop --host $1 --db $2 ./$2
+mongorestore --batchSize=10 --drop --host $1 --db $2 ./$2 "${@:3}"
 rm -rf ./$2
 echo "File /backups/$2/latest.tar.gz was restored into $2 database."
